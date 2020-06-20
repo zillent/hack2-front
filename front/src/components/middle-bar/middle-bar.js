5f0bb8166bf8dc400389fbba1bdef24e12c9ad58
Sergey Kozlov <serg@zillent.ru>
2020-06-20 22:59:25 +0500

    disable pictures and check if person id exists

--- front/src/components/middle-bar/middle-bar.js
+++ front/src/components/middle-bar/middle-bar.js
@@ -7,28 +7,22 @@ import OfferItem from "../offer-item";
 class MiddleBar extends Component {
   apiService = new APIService();
   offersURL = "/api/v0/offer/";
-  usersURL = "/users_api/v1_0/person/";
   state = { offers: [] };
 
   componentDidMount() {
     try {
       this.apiService.getItems(this.offersURL).then((result) => {
         this.setState({ offers: result });
-        result.map((item)=>{
-        setTimeout(()=>this.apiService.getItems(this.usersURL+item.id).then((user) => {
-            console.log(user);
-        })
-        ,0);
-    })
       });
     } catch (error) {
       console.log("Error!");
     }
   }
+
   render(props) {
     let offers = this.state.offers;
     if (offers.length === 0 && window.location.hostname === "localhost") {
-        console.log('Using Fake data!')
+      console.log("Using Fake data!");
       // FAKE DATA for testing!
       offers = [
         {
@@ -39,24 +33,24 @@ class MiddleBar extends Component {
           avatar:
             "http://127.0.0.1:8822/v0/offer/viber_image_2019-04-02__21.58.21.jpg",
           create_duid: "2020-06-20T06:14:21.277000Z",
-          person: 1,
+          person_id: 1,
         },
         {
-            id: 2,
-            offer_type_name: "Организовать доставку воды в офис",
-            detail:
-              "Организовать доставку воды в офис с формированием фонда запаса воды,",
-            avatar:
-              "http://127.0.0.1:8822/v0/offer/viber_image_2019-04-02__21.58.21.jpg",
-            create_duid: "2020-06-20T06:14:21.277000Z",
-            person: 3,
-          },
-        ];
+          id: 2,
+          offer_type_name: "Организовать доставку воды в офис",
+          detail:
+            "Организовать доставку воды в офис с формированием фонда запаса воды,",
+          avatar:
+            "http://127.0.0.1:8822/v0/offer/viber_image_2019-04-02__21.58.21.jpg",
+          create_duid: "2020-06-20T06:14:21.277000Z",
+          person_id: 3,
+        },
+      ];
     }
 
     if (offers && offers.length > 0) {
       return (
-        <div className='wind-'>
+        <div className="wind-">
           {offers.map((offer) => (
             <OfferItem data={offer} key={offer.id}></OfferItem>
           ))}
--- front/src/components/offer-item/offer-item.js
+++ front/src/components/offer-item/offer-item.js
@@ -1,25 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import APIService from "../../services/service";
 import "./offer-item.css";
 
-const OfferItem = (props) => {
-  return (
-    <div className="offer-item__container">
-      <Row className ="offer-item__header">
-        <Col>{props.data.offer_type_name}</Col>
-      </Row>
-      <Row className ="offer-item__body">
-        <Col>{props.data.detail}</Col>
-      </Row>
+class OfferItem extends Component {
+  apiService = new APIService();
+  usersURL = "/users_api/v1_0/person/";
+  state = { Person: [] };
 
-      <Row className ="offer-item__footer">
-        <Col>
-          <img src={props.data.avatar} alt="Offer avatar"></img>
-        </Col>
-      </Row>
-    </div>
-  );
-};
+  componentDidMount() {
+    console.log(this.props);
+    if (this.props.data.person_id) {
+      let url = this.usersURL + this.props.data.person_id + "/";
+      console.log(url);
+      this.apiService.getItems(url).then((usr) => {
+        console.log(usr);
+      });
+    }
+  }
+  
+  render() {
+    return (
+      <div className="offer-item__container">
+        <Row className="offer-item__header">
+          <Col>{this.props.data.offer_type_name}</Col>
+        </Row>
+        <Row className="offer-item__body">
+          <Col>{this.props.data.detail}</Col>
+        </Row>
+
+        <Row className="offer-item__footer">
+          <Col className="d-none">
+            <img src={this.props.data.avatar} alt="Offer avatar"></img>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
 
 export default OfferItem;
